61740bc14ca15dd12eaf720b02008b0246bc0290
Quang <quang29112002@gmail.com>
2022-06-21 20:53:01 +0700

    zoo.java nationalpark.java

--- VnTourism/src/main/java/hust/soict/globalict/entity/tourism/natural_attraction/park/Zoo.java
+++ VnTourism/src/main/java/hust/soict/globalict/entity/tourism/natural_attraction/park/Zoo.java
@@ -1,5 +1,36 @@
 package hust.soict.globalict.entity.tourism.natural_attraction.park;
+import hust.soict.globalict.entity.rdf.Prefix;
 
-public class Zoo extends Park{
+public class Zoo extends Park {
+    public final static String OBJECT_ZOO = "dbc:Zoos_in_Vietnam.";
 
+    public Zoo() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.createRawTtlFile(OBJECT_ZOO);
+	}
+
+    @Override
+    public String createSparqlQuery() {
+		return Prefix.PREFIX + "CONSTRUCT{\r\n"
+				+ "?place dbo:wikiPageWikiLink "+ OBJECT_ZOO + "\r\n"
+				+ this.getName()+"\r\n"
+				+ this.getComment()+"\r\n"
+				+ this.getGeoPoint()+"\r\n"
+				+ this.getGeoLat()+"\r\n"
+				+ this.getGeoLong()+"\r\n"
+				+ this.getLocation()+"\r\n"
+				+ this.getCountry()+"\r\n"
+				+ "} WHERE{\r\n"
+				+ "?place dbo:wikiPageWikiLink "+ OBJECT_ZOO + "\r\n"
+				+ "OPTIONAL {"+ this.getName()+"}\r\n"
+				+ "OPTIONAL {"+ this.getComment()+"}\r\n"
+				+ "OPTIONAL {"+ this.getGeoPoint()+"}\r\n"
+				+ "OPTIONAL {"+ this.getGeoLat()+"}\r\n"
+				+ "OPTIONAL {"+ this.getGeoLong()+"}\r\n"
+				+ "OPTIONAL {"+ this.getLocation()+"}\r\n"
+				+ "OPTIONAL {"+ this.getCountry()+"}\r\n"
+				+ "FILTER ( LANG ( ?comment ) = 'en' ).\r\n"
+				+ "}";
+	}
 }
