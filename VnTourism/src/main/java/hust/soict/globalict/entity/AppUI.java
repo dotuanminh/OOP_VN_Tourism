2b988261993571b4d0b270ef2f190094de8500ca
Nguyễn Hoàng Hải <hai.nh200193@sis.hust.edu.vn>
2022-06-29 20:28:49 +0700

    Extends UI

--- VnTourism/src/main/java/hust/soict/globalict/entity/AppUI.java
+++ VnTourism/src/main/java/hust/soict/globalict/entity/AppUI.java
@@ -1,7 +1,11 @@
 package hust.soict.globalict.entity;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -12,8 +16,12 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Optional;
+
+
 
 public class AppUI extends Application {
+
 
     private ChoiceBox<String> manmadeChoiceBox = new ChoiceBox<String>();
     private ChoiceBox<String> naturalChoiceBox = new ChoiceBox<String>();
@@ -36,11 +44,11 @@ public class AppUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         Text title = new Text("Welcome to Vietnam tourism");
-        title.setFont(Font.font("Calibri",FontWeight.MEDIUM,20));
+        title.setFont(Font.font("Calibri",FontWeight.MEDIUM,24));
         ToggleGroup toggleGroup = new ToggleGroup();
 
-        RadioButton manmadeButton = new RadioButton();
-        RadioButton naturalButton = new RadioButton();
+        RadioButton manmadeButton = new RadioButton("Manmade Attraction");
+        RadioButton naturalButton = new RadioButton("Natural Attraction");
 
         manmadeButton.setToggleGroup(toggleGroup);
         naturalButton.setToggleGroup(toggleGroup);
@@ -48,22 +56,85 @@ public class AppUI extends Application {
         manmadeChoiceBox.getItems().addAll(manmadeAttraction);
         naturalChoiceBox.getItems().addAll(naturalAttraction);
 
+        manmadeChoiceBox.getSelectionModel().selectFirst();
+        naturalChoiceBox.getSelectionModel().selectFirst();
+
+        manmadeChoiceBox.setDisable(true);
+        naturalChoiceBox.setDisable(true);
+
+        Button buttonGet = new Button("Get");
+        buttonGet.setDisable(true);
+        Button buttonExit = new Button("Exit");
+
+        toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                buttonGet.setDisable(false);
+                RadioButton rb = (RadioButton) t1;
+                if(rb.getText().equals(manmadeButton.getText())){
+                    manmadeChoiceBox.setDisable(false);
+                    manmadeChoiceBox.show();
+                    naturalChoiceBox.setDisable(true);
+                }
+                else if(rb.getText().equals(naturalButton.getText())){
+                    naturalChoiceBox.setDisable(false);
+                    naturalChoiceBox.show();
+                    manmadeChoiceBox.setDisable(true);
+                }
+
+            }
+        });
+
+
+
+        buttonGet.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.initStyle(StageStyle.UTILITY);
+                alert.setTitle("Confirmation");
+                RadioButton rb = (RadioButton) toggleGroup.getSelectedToggle();
+                String chosen = null;
+                if(rb.getText().equals(manmadeButton.getText())){
+                    chosen = manmadeChoiceBox.getValue();
+                }
+                else if (rb.getText().equals(naturalButton.getText())){
+                    chosen = naturalChoiceBox.getValue();
+                }
+                alert.setHeaderText("Is this your choice?");
+                alert.setContentText("You have selected " + rb.getText() + " - " + chosen);
+                Optional<ButtonType> option = alert.showAndWait();
+                if(option.get() == ButtonType.OK){
+                    // needs processing here
+                }
+
+            }
+        });
+
+        buttonExit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
+            }
+        });
+
         GridPane gridPane = new GridPane();
         gridPane.setMinSize(400,200);
-        gridPane.setPadding(new Insets(10,10,10,10));
+//        gridPane.setPadding(new Insets(20,20,20,20));
 
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
+        gridPane.setHgap(10);
+        gridPane.setVgap(30);
         gridPane.setAlignment(Pos.CENTER);
 
-        gridPane.add(title,1,0);
+        gridPane.add(title,0,0);
         gridPane.add(manmadeButton,0,1);
-        gridPane.add(new Text("Manmade attraction"),1,1);
-        gridPane.add(manmadeChoiceBox,2,1);
+
+        gridPane.add(manmadeChoiceBox,1,1);
         gridPane.add(naturalButton,0,2);
-        gridPane.add(new Text("Natural attraction"),1,2);
-        gridPane.add(naturalChoiceBox,2,2);
-        gridPane.add(new Button("Get"),1,5);
+
+        gridPane.add(naturalChoiceBox,1,2);
+        gridPane.add(buttonGet,0,3);
+        gridPane.add(buttonExit,1,3);
 
         Scene scene = new Scene(gridPane,500,300);
 
@@ -71,7 +142,7 @@ public class AppUI extends Application {
 
         primaryStage.setTitle("Vietnam Tourism");
         primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
 
 
